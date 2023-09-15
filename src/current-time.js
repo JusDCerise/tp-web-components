a886d5643b3d2962cfe16530026041ca67104279
Guillaume <guillaume.farbos33@gmail.com>
2023-09-15 13:43:45 +0200

    upd: tp part 1

---
+++ src/current-time.js
@@ -0,0 +1,45 @@
+class CurrentTime extends HTMLElement {
+  static get observedAttributes() {
+    return ["format"];
+  }
+
+  attributeChangedCallback(attr, oldVal, newVal) {
+    if(attr === "format"){
+        this.format = newVal;
+    }
+    if(this.$title){
+        this.render();
+    }
+  }
+
+  connectedCallback() {
+    this.innerHTML = `<div class="currentTime">
+    <p class="currentTime__title"></p>
+    <time class="currentTime__time"></time>`;
+    this.$title = this.querySelector(".currentTime__title");
+    this.$time = this.querySelector(".currentTime__time");
+    this.render();
+    this.interval = setInterval(() => {
+      this.renderTime();
+    }, 1000);
+  }
+
+  disconnectedCallback() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    this.$title.innerHTML = this.format === "utc" ? "Heure UTC" : "Heure locale";
+    this.renderTime();
+  }
+
+  renderTime() {
+    const date = new Date();
+    this.$time.innerHTML =
+      this.format === "utc" ? date.toUTCString() : date.toLocaleString();
+    this.$time.setAttribute("datetime", date.toISOString());
+    this.rendered = true;
+  }
+}
+
+customElements.define("current-time", CurrentTime);
